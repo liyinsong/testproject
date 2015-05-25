8c673955437fcc43e08d3669f4f7c4ff27ca71d1
eric <eric@ADMINIB-D6B1TBI>
2015-05-25 11:05:27 +0800

    add event demo test
    Signed-off-by: eric <eric@ADMINIB-D6B1TBI>

---
+++ src/lys/test/event/MainTest.java
@@ -0,0 +1,18 @@
+package lys.test.event;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		EventSourceObject object = new EventSourceObject();
+		// 注册监听器
+		object.addCusListener(new CusEventListener() {
+			@Override
+			public void fireCusEvent(CusEvent e) {
+				super.fireCusEvent(e);
+			}
+		});
+		// 触发事件
+		object.setName("eric");
+	}
+
+}
